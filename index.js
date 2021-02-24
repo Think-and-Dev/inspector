514dd53ce40b89279aa5bd931763f0caea3db89d
Manflack <marianofauez@live.com>
2021-02-24 17:46:58 -0300

    constants and improves

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 const Mongo = require("./Mongo");
 const Middleware = require("./middleware/middleware");
 
-/*
-console.log("Test mongoDB!!\n");
-const testMongo = new Mongo();
-testMongo.createConnection();
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-testMongo.saveAndFlush({test: "description test"}).catch(console.dir);
-*/
-
-
 const middleware = new Middleware();
 middleware.getAccountUnderwater();
