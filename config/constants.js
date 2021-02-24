d4e856141d26e0746555fa3dd46c784eafd558db
Manflack <marianofauez@live.com>
2021-02-24 17:46:58 -0300

    constants and improves

--- config/constants.js
+++ config/constants.js
@@ -3,6 +3,8 @@ const HTTP_PROVIDER = process.env.VUE_APP_HTTP_PROVIDER || "http://18.218.165.23
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mongodb";
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "rlending";
 const LIQUIDATOR_ADDRESS = process.env.LIQUIDATOR_ADDRESS || "0xef558B499B7fDeB387c3a3A42E2d414815c64fac";
+const INITIAL_BLOCK = process.env.INITIAL_BLOCK || 1504046;
+const CRBTC_SYMBOL = "cRBTC";
 
 module.exports = {
   NETWORK_ID,
@@ -10,4 +12,6 @@ module.exports = {
   MONGO_URI,
   MONGO_DB_NAME,
   LIQUIDATOR_ADDRESS,
+  INITIAL_BLOCK,
+  CRBTC_SYMBOL
 };
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,7 +3,11 @@ const Mongo = require("../Mongo");
 const BigNumber = require("bignumber.js");
 const { ethers } = require("ethers");
 const { abi, cTokensDetails, constants } = require("./constants");
-const { HTTP_PROVIDER } = require("../config/constants");
+const {
+  HTTP_PROVIDER,
+  INITIAL_BLOCK,
+  CRBTC_SYMBOL,
+} = require("../config/constants");
 
 BigNumber.set({ EXPONENTIAL_AT: [-18, 36] });
 
@@ -11,21 +15,21 @@ class Middleware {
   constructor() {
     this.factoryContractInstance = new FactoryContract();
     this.mongoInstance = new Mongo();
+    this.provider = new ethers.providers.JsonRpcProvider(HTTP_PROVIDER);
   }
 
   async borrowAccountsByMarket(isCRBTC, contractInstance, contractSymbol) {
     let borrows = [];
 
     if (!HTTP_PROVIDER) return borrows;
-    const provider = new ethers.providers.JsonRpcProvider(HTTP_PROVIDER);
 
     const abiCtoken = isCRBTC ? abi["cRBTC"] : abi["cErc20"];
     const filterLocal = contractInstance.filters.Borrow();
-    const latest = await provider.getBlockNumber();
-    const ini = 1504046;
-    for (let index = latest; index > ini; index -= 1000) {
+    const latest = await this.provider.getBlockNumber();
+
+    for (let index = latest; index > INITIAL_BLOCK; index -= 1000) {
       try {
-        let logs = await provider.getLogs({
+        let logs = await this.provider.getLogs({
           ...filterLocal,
           fromBlock: index - 1000,
           toBlock: index,
@@ -36,7 +40,7 @@ class Middleware {
             const { borrowAmount } = iface.parseLog(element).args;
             return {
               address: iface.parseLog(element).args[0],
-              borrowAmount: new BigNumber(borrowAmount._hex.toString())
+              borrowAmount: new BigNumber(borrowAmount.toString())
                 .div(1e18)
                 .toString(),
               marketBorrowed: contractSymbol,
@@ -64,7 +68,7 @@ class Middleware {
     //iterate all market by one and push the elements
     for (let i = 0; i < cTokensDetails.length; i++) {
       const borrowAccountsByMarket = await this.borrowAccountsByMarket(
-        cTokensDetails[i].symbol === "cRBTC",
+        cTokensDetails[i].symbol === CRBTC_SYMBOL,
         this.factoryContractInstance.getContractCtoken(
           cTokensDetails[i].symbol
         ),
@@ -81,7 +85,9 @@ class Middleware {
       );
 
       const actualIsRed =
-        new BigNumber(liquidity.accountShortfall._hex).isGreaterThan(0) === true
+        new BigNumber(liquidity.accountShortfall.toString()).isGreaterThan(
+          0
+        ) === true
           ? true
           : false;
 
